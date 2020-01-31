aa0fd057345d3bc7ee78e4f3876b5018a861e3d6
piechu007 <534405691+piechu007@users.noreply.github.com>
2020-01-31 21:40:38 +0100

    Spawner statyczny

---
+++ Assets/Scripts/enviro/Meteor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meteor : MonoBehaviour
+{
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Setvelocity(Vector3 direction)
+    {
+        rb.velocity = 
+    }
+}
---
+++ Assets/Scripts/enviro/Spawner.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject prifab;
+    [SerializeField]
+    int maxCount = 10;
+
+    [SerializeField]
+    float angle = 15;
+    [SerializeField]
+    float range = 10;
+    [SerializeField]
+    float[] timeToNextSpawn = { 1, 0, 3, 2, 1, 2 };
+
+    GameObject[] list;
+    float time = 0;
+
+    void Start()
+    {
+        list = new GameObject[maxCount];
+        for(int i=0; i<maxCount; i++)
+        {
+            list[i] = GameObject.Instantiate(prifab, new Vector3(100,100,100), Quaternion.identity,null);
+        }
+        ResetTime();
+    }
+
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if (time < 0)
+        {
+            ResetTime();
+            Spawn();
+        }
+    }
+
+    //Random.value od 0 do 1
+    void Spawn()
+    {
+        Vector2 direction = new Vector2(Random.value-0.5f, Random.value - 0.5f);
+        direction = direction.normalized * range;
+        Debug.Log(direction);
+        int maxIndex = FindFarthest();
+        list[maxIndex].transform.position = transform.position + (Vector3)direction;
+    }
+
+    int FindFarthest()
+    {
+        int maxIndex = 0;
+        float maxDistance = Vector2.Distance(transform.position,list[0].transform.position);
+        for(int i = 1; i < list.Length; i++)
+        {
+            float distance = Vector2.Distance(transform.position, list[0].transform.position);
+            if (Vector2.Distance(transform.position, list[maxIndex].transform.position) < Vector2.Distance(transform.position, list[i].transform.position))
+            {
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
+    }
+
+    void ResetTime()
+    {
+        int rand = Random.RandomRange(0, timeToNextSpawn.Length - 1);
+        time = timeToNextSpawn[rand];
+    }
+}
