6f76ad4fca06fc2c8433f6ab480ad469cb07f8c2
Andrzej Zabski <jedrak32@gmail.com>
2020-02-01 00:09:12 +0100

    Dock system prototipe

--- Assets/Scripts/Input/DropDown.cs
+++ Assets/Scripts/Input/DropDown.cs
@@ -5,20 +5,40 @@ using UnityEngine.EventSystems;
 
 public class DropDown : MonoBehaviour
 {
+    public Vector3 startSpeed, acceleration;
+    private bool _isDraged;
+    private Rigidbody2D _rb;
 
-
-    private void OnMouseDown()
+    private void Start()
     {
-        Debug.Log("MouseClick");
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     private void OnMouseDrag()
     {
+        _isDraged = true;
         Vector3 buff = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = new Vector3(buff.x, buff.y, 0);
-        Debug.Log(transform.position);
+        _rb.velocity = new Vector2(startSpeed.x, startSpeed.y);
     }
 
+    private void OnMouseUp()
+    {
+        _isDraged = false;
+    }
 
+    private void Update()
+    {
+        if (!_isDraged)
+        {
+            _rb.velocity +=  new Vector2 (acceleration.x, acceleration.y);
+        }
+        else
+        {
+            _rb.velocity = Vector2.zero;
+        }
+    
+        _rb.angularVelocity = startSpeed.magnitude * 50;
+    }
 
 }
---
+++ Assets/Scripts/Ship/DockHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DockHandler : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject _dock;
+
+    private void attach(GameObject dock)
+    {
+        _dock = dock;
+    }
+
+    private void dettach()
+    {
+        _dock = null;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Dock")
+        {
+            _dock = collision.gameObject;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = _dock.transform.position;
+        transform.rotation = _dock.transform.rotation;
+    }
+}
