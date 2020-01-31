cfb289629f68b556045edc8b31d7648bb0192f71
Andrzej Zabski <jedrak32@gmail.com>
2020-01-31 23:13:04 +0100

    drag and drop and docks

---
+++ Assets/Scripts/Input/DropDown.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropDown : MonoBehaviour
+{
+
+
+    private void OnMouseDown()
+    {
+        Debug.Log("MouseClick");
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 buff = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(buff.x, buff.y, 0);
+        Debug.Log(transform.position);
+    }
+
+
+
+}
